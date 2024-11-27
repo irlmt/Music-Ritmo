6b5122748fba4739d2b1293dd7d82725d10e8efc
Sofya Fedorova <sofa888003@gmail.com>
2024-11-28 01:32:06 +0300

    [mus-10, mus-11]: Created buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import Logo from "../../public/images/logo.svg";
 
+import { Button } from "@/shared/button";
+
 export default function Home() {
   return (
     <div>
@@ -9,6 +11,15 @@ export default function Home() {
       <p>Добро пожаловать в musicRitmo!</p>
       <Image src={Logo} alt="Логотип musicRitmo" width={100} height={100} />
       <i className="fa-regular fa-heart"></i>
+      <Button type="normal" color="green" disabled={true}>
+        вход
+      </Button>
+      <Button type="normal" color="white" disabled={true}>
+        вход
+      </Button>
+      <Button type="transparent" color="green-text">
+        вход
+      </Button>
     </div>
   );
 }
