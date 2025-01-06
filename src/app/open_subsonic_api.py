82ef573038cf1fef63ba20518b9cc40c73ff998c
Denis Borisov <defdden@mail.ru>
2025-01-06 19:53:10 +0300

    [mus-108] Create open_subsonic_router, add ping endpoint

---
+++ src/app/open_subsonic_api.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter
+from fastapi.responses import JSONResponse
+
+open_subsonic_router = APIRouter(prefix="/rest")
+
+
+class SubsonicResponse:
+    def __init__(self):
+        self.data = {}
+        self.data["status"] = "ok"
+        self.data["version"] = "1.16.1"
+        self.data["type"] = "MusicRitmo"
+        self.data["serverVersion"] = "0.1"
+        self.data["openSubsonic"] = True
+
+    def to_json_rsp(self) -> JSONResponse:
+        return JSONResponse({"subsonic-response": self.data})
+
+
+@open_subsonic_router.get("/ping")
+async def ping():
+    rsp = SubsonicResponse()
+    return rsp.to_json_rsp()
