1866f35d52667bf3df384b3f425d2b3f3a065e65
Sofya Fedorova <sofa888003@gmail.com>
2025-01-20 20:44:35 +0300

    [mus-16]: Created ui search panel

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Header } from "@/widgets/header";
+import { SearchPanel } from "@/features/search-panel";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <SearchPanel />
     </>
   );
 }
