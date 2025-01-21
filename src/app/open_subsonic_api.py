f2ee2b046ac8cf8a6451dbd6920bccaa7ba8ad87
BenSerg <bendryshev.sa@yandex.ru>
2025-01-22 02:05:11 +0300

    formatted line

--- src/app/open_subsonic_api.py
+++ src/app/open_subsonic_api.py
@@ -66,39 +66,30 @@ def get_tracks_by_genre(genre_name: str, session: Session = Depends(db.get_sessi
     for track in genre.tracks:
         track_info = SubsonicTrack(
             id=str(track.id) if hasattr(track, 'id') else SubsonicTrack.model_fields['id'].default,
-            parent=str(track.parent_id) if hasattr(track, 'parent_id') else SubsonicTrack.model_fields[
-                'parent'].default,
+            parent=str(track.parent_id) if hasattr(track, 'parent_id') else SubsonicTrack.model_fields['parent'].default,
             title=track.title if hasattr(track, 'title') else SubsonicTrack.model_fields['title'].default,
             isDir=track.is_dir if hasattr(track, 'is_dir') else SubsonicTrack.model_fields['isDir'].default,
             isVideo=track.is_video if hasattr(track, 'is_video') else SubsonicTrack.model_fields['isVideo'].default,
             type=track.type if hasattr(track, 'type') else SubsonicTrack.model_fields['type'].default,
-            albumId=str(track.album_id) if hasattr(track, 'album_id') else SubsonicTrack.model_fields[
-                'albumId'].default,
+            albumId=str(track.album_id) if hasattr(track, 'album_id') else SubsonicTrack.model_fields['albumId'].default,
             album=str(track.album) if hasattr(track, 'album') else SubsonicTrack.model_fields['album'].default,
-            artistId=str(track.artist_id) if hasattr(track, 'artist_id') else SubsonicTrack.model_fields[
-                'artistId'].default,
+            artistId=str(track.artist_id) if hasattr(track, 'artist_id') else SubsonicTrack.model_fields['artistId'].default,
             artist=track.artist if hasattr(track, 'artist') else SubsonicTrack.model_fields['artist'].default,
             coverArt=track.cover_art if hasattr(track, 'cover_art') else SubsonicTrack.model_fields['coverArt'].default,
             duration=track.duration if hasattr(track, 'duration') else SubsonicTrack.model_fields['duration'].default,
             bitRate=track.bit_rate if hasattr(track, 'bit_rate') else SubsonicTrack.model_fields['bitRate'].default,
             bitDepth=track.bit_depth if hasattr(track, 'bit_depth') else SubsonicTrack.model_fields['bitDepth'].default,
-            samplingRate=track.sampling_rate if hasattr(track, 'sampling_rate') else SubsonicTrack.model_fields[
-                'samplingRate'].default,
-            channelCount=track.channel_count if hasattr(track, 'channel_count') else SubsonicTrack.model_fields[
-                'channelCount'].default,
-            userRating=track.user_rating if hasattr(track, 'user_rating') else SubsonicTrack.model_fields[
-                'userRating'].default,
-            averageRating=track.average_rating if hasattr(track, 'average_rating') else SubsonicTrack.model_fields[
-                'averageRating'].default,
+            samplingRate=track.sampling_rate if hasattr(track, 'sampling_rate') else SubsonicTrack.model_fields['samplingRate'].default,
+            channelCount=track.channel_count if hasattr(track, 'channel_count') else SubsonicTrack.model_fields['channelCount'].default,
+            userRating=track.user_rating if hasattr(track, 'user_rating') else SubsonicTrack.model_fields['userRating'].default,
+            averageRating=track.average_rating if hasattr(track, 'average_rating') else SubsonicTrack.model_fields['averageRating'].default,
             track=track.track_number if hasattr(track, 'track_number') else SubsonicTrack.model_fields['track'].default,
             year=track.year if hasattr(track, 'year') else SubsonicTrack.model_fields['year'].default,
             genre=track.genre if hasattr(track, 'genre') else SubsonicTrack.model_fields['genre'].default,
             size=track.size if hasattr(track, 'size') else SubsonicTrack.model_fields['size'].default,
-            discNumber=track.disc_number if hasattr(track, 'disc_number') else SubsonicTrack.model_fields[
-                'discNumber'].default,
+            discNumber=track.disc_number if hasattr(track, 'disc_number') else SubsonicTrack.model_fields['discNumber'].default,
             suffix=track.suffix if hasattr(track, 'suffix') else SubsonicTrack.model_fields['suffix'].default,
-            contentType=track.content_type if hasattr(track, 'content_type') else SubsonicTrack.model_fields[
-                'contentType'].default,
+            contentType=track.content_type if hasattr(track, 'content_type') else SubsonicTrack.model_fields['contentType'].default,
             path=track.path if hasattr(track, 'path') else SubsonicTrack.model_fields['path'].default
         )
         tracks_data.append(track_info.model_dump())
