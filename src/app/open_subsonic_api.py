2f138189a9d7f345851f915372ad678b1b36da01
Batueva Vlada <irelmtt@gmail.com>
2025-01-22 00:39:31 +0300

    [mus-136] Add getPlaylists endpoint

--- src/app/open_subsonic_api.py
+++ src/app/open_subsonic_api.py
@@ -1,5 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from fastapi.responses import JSONResponse
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlmodel import select
+from . import database as db
 
 open_subsonic_router = APIRouter(prefix="/rest")
 
@@ -20,4 +23,30 @@ class SubsonicResponse:
 @open_subsonic_router.get("/ping")
 async def ping():
     rsp = SubsonicResponse()
+    return rsp.to_json_rsp()
+
+@open_subsonic_router.get("/getPlaylists")
+async def get_playlists(session: AsyncSession = Depends(db.get_session)):
+    rsp = SubsonicResponse()
+
+    try:
+        result = await session.execute(select(db.Playlist))
+        playlists = result.scalars().all()
+
+        rsp.data["playlists"] = {
+            "playlist": [
+                {
+                    "id": playlist.id,
+                    "name": playlist.name,
+                    "owner": playlist.user_id,
+                    "songCount": playlist.total_tracks,
+                    "created": playlist.create_date,
+                }
+                for playlist in playlists
+            ]
+        }
+    except Exception as e:
+        rsp.data["status"] = "failed"
+        rsp.data["error"] = str(e)
+
     return rsp.to_json_rsp()
