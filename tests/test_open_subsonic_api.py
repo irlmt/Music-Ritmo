65936f7ef78bb538497c04f8b6e3ccbf77366543
Batueva Vlada <irelmtt@gmail.com>
2025-01-22 01:40:18 +0300

    [mus-136] Add test for getPlaylists

--- tests/test_open_subsonic_api.py
+++ tests/test_open_subsonic_api.py
@@ -14,3 +14,28 @@ def test_ping():
     assert rsp["openSubsonic"] == True
     assert rsp["version"] is not None
     assert rsp["serverVersion"] is not None
+
+
+def test_getPlaylists():
+    response = client.post("/playlists", json={
+        "name": "My Playlist",
+        "user_id": 1,
+        "total_tracks": 10,
+        "create_date": "2025-01-01"
+    })
+    assert response.status_code == 200
+
+    response = client.get("/rest/getPlaylists")
+    assert response.status_code == 200
+
+    data = response.json()
+
+    subsonic_response = data["subsonic-response"]
+    playlists = subsonic_response["playlists"]["playlist"]
+    assert len(playlists) > 0
+
+    playlist = playlists[0]
+    assert playlist["name"] == "My Playlist"
+    assert playlist["owner"] == 1
+    assert playlist["songCount"] == 10
+    assert playlist["createDate"] == "2025-01-01"
