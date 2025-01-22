7279409952734746994ffe16dc56daa53064b7ee
Denis Borisov <defdden@mail.ru>
2025-01-22 11:21:12 +0300

    Setup CORS policy

--- src/app/main.py
+++ src/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .database import init_db
 from .db_endpoints import router
 from .open_subsonic_api import open_subsonic_router
@@ -6,6 +7,18 @@ from .db_loading import scan_directory_for_audio_files, load_audio_data
 from .frontend_endpoints import frontend_router
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 init_db()
 
