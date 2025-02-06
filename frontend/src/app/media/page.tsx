7d60b30fd05e8b3dc190b474a078da77403ca646
Sofya Fedorova <sofa888003@gmail.com>
2025-02-06 22:19:40 +0300

    [mus-25]: Created scanning

--- frontend/src/app/media/page.tsx
+++ frontend/src/app/media/page.tsx
@@ -1,12 +1,35 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import { Container } from "@/shared/container";
 import { Button } from "@/shared/button";
+import { useState } from "react";
 import styles from "./media.module.css";
 
 export default function Media() {
-  const router = useRouter();
+  const [scanStatus, setScanStatus] = useState("");
+
+  const startScan = async () => {
+    try {
+      const response = await fetch("http://localhost:8000/rest/startScan", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const data = await response.json();
+
+      if (response.ok && data["subsonic-response"].status === "ok") {
+        setScanStatus("Сканирование выполнено успешно!");
+        setTimeout(() => setScanStatus(""), 1500);
+      } else {
+        setScanStatus("Ошибка при запуске сканирования");
+      }
+    } catch (error) {
+      console.error("Error during scan initiation:", error);
+      setScanStatus("Ошибка при запуске сканирования");
+    }
+  };
 
   return (
     <>
@@ -21,19 +44,15 @@ export default function Media() {
         arrow={true}
         link_arrow="/"
       >
-        <h1 className={styles.create_playlists__title}>Медиатека</h1>
+        <h1>Медиатека</h1>
 
         <div style={{ display: "flex", justifyContent: "center" }}>
-          <Button
-            type="normal"
-            color="green"
-            onClick={() => {
-              router.push("/");
-            }}
-          >
-            запустить сканирование
+          <Button type="normal" color="green" onClick={startScan}>
+            Запустить сканирование
           </Button>
         </div>
+
+        {scanStatus && <div className={styles.message}>{scanStatus}</div>}
       </Container>
     </>
   );
