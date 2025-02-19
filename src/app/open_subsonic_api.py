6d8698f335628f008aca0309b2034093d3b3d85d
shilkon <sh1lkon@yandex.ru>
2025-02-19 20:48:38 +0300

    fixed black formatting

--- src/app/open_subsonic_api.py
+++ src/app/open_subsonic_api.py
@@ -635,7 +635,9 @@ def get_cover_art(
         image.thumbnail((size, size))
         image_bytes = utils.image_to_bytes(image)
 
-    return Response(content=image_bytes, media_type=f"image/{(image.format or "").lower()}")
+    return Response(
+        content=image_bytes, media_type=f"image/{(image.format or "").lower()}"
+    )
 
 
 @open_subsonic_router.get("/getAvatar")
