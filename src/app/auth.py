593ff7425704a3515193f7a036a24fae36debae6
Denis Borisov <defdden@mail.ru>
2025-03-20 11:09:12 +0300

    [mus-231] Add hex password encoding support

--- src/app/auth.py
+++ src/app/auth.py
@@ -4,12 +4,29 @@ from sqlmodel import Session, select
 from . import database as db
 
 
+def decode_password(p: str) -> tuple[str | None, bool]:
+    password: str = ""
+    if p.startswith("enc"):
+        if len(p) < 4:
+            return None, True
+        enc_pass = bytes.fromhex(p[4:])
+        password = enc_pass.decode("utf-8")
+    else:
+        password = p
+
+    return password, False
+
+
 def authenticate_user(
     u: str = Query(None),
     p: str = Query(None),
     session: Session = Depends(db.get_session),
 ) -> db.User:
     user = session.exec(select(db.User).where(db.User.login == u)).first()
-    if not user or user.password != p:
+
+    password, err = decode_password(p)
+
+    if not user or err or user.password != password:
         raise HTTPException(status_code=401, detail="Wrong username or password")
+
     return user
